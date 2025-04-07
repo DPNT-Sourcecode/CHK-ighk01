e313b0f170a5ceb49229ea81ffe4b9a474af84fe
sbx_user1051 <sbx_user1051@169.254.6.233>
2025-04-07 13:39:15 +0000

    Mon Apr 07 13:39:15 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,10 +9,15 @@ class CheckoutSolution:
             return -1
             
         # Initialize the price table
-        price_table = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
+        price_table = {
+            'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10,
+            'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
+            'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
+            'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
+            'Y': 10, 'Z': 50
+        }
         
-        # Initialize multi-buy offers (item: [(quantity, price), ...])
-        # Offers are sorted in descending order of quantity for better customer benefit
+        
         multi_buy_offers = {
             'A': [(5, 200), (3, 130)],
             'B': [(2, 45)]
@@ -82,5 +87,6 @@ class CheckoutSolution:
                 total_price += count * price_table[item]
         
         return total_price
+
 
 
