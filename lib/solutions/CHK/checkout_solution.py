978bfad0caee6df0772c2a08423fb8e16340bf44
sbx_user1051 <sbx_user1051@169.254.6.233>
2025-04-07 13:30:42 +0000

    Mon Apr 07 13:30:42 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,7 +11,10 @@ class CheckoutSolution:
 
         # Initialize the price table and special offers
         price_table = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-        special_offers = {'A': (3, 130), 'B': (2, 45), 'E': (2,40, 'B')}
+        special_offers = {'A': (3, 130), 'B': (2, 45), 'E': (2, 40, 'B')}
+        special_offers['A'] = (5, 200)
+        
+
 
         total_price = 0
         item_counts = {}
@@ -34,3 +37,4 @@ class CheckoutSolution:
                 total_price += count * price_table[item]
         return total_price
         
+
